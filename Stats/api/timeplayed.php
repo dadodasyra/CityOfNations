1fe22358640baa0ac3dd5c8be78c2651596fa685
dadodasyra <dadodasyra@users.noreply.github.com>
2021-08-09 15:35:16 +0200

    Stop timeplayed update Update team list Put auto refresh on timeplayed Remove Connected since for the end Add days to formatdata Update html background color Add end category Replace XML with fetch in streamhack version

--- Stats/api/timeplayed.php
+++ Stats/api/timeplayed.php
@@ -1,7 +1,7 @@
 <?php
 
 $file = file_get_contents("cache.json", true);
-$dejsoned = json_decode($file, true);
+/*$dejsoned = json_decode($file, true);
 if(($dejsoned["expire"]??0) < time()){
     $request = file_get_contents("https://mcsharp.fr/api/stats");
     echo $request;
@@ -9,6 +9,6 @@ if(($dejsoned["expire"]??0) < time()){
     $decoderequest = json_decode($request, true);
     $decoderequest["expire"] = time() + 30;
     file_put_contents("cache.json", json_encode($decoderequest));
-} else {
+} else {*/
     echo $file;
-}
+//}
